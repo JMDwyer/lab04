3cccd2f9f89526406cbde9d7e03a311982611346
Joseph M Dwyer <joe@dhcp-rhodes-2490.redrover.cornell.edu>
2017-11-11 17:10:31 -0500

    Block diagram w/ TODO's

--- enc.m
+++ enc.m
@@ -1,34 +1,20 @@
 function x = enc(bits)
-
-   % number of bits to send; 'bits' should be a column vector of
-   % this size
-   numbits = 24;
+   
    % power constraint
    P = 1/4;
-   % time constant of the channel: approx. number of nonzero samples
-   % in the impulse response
-   K = 250;
+   numbits = length(bits);
+   %BITS ARE DIVIDED IN AUDIOWRITE
+   %K is the bits/sample   
+   K = 24;
 
-   % the length of our transmission. After sending one bit, we
-   % wait for the channel to settle down before sending another,
-   % so it takes numbits*K samples overall.
-   n = numbits*K;
+   %OFDM STEPS
+   %STEP 1, TAKE FFT OF BITS
+   X_fft = fft(bits);
+   
+   %APPEND CYCLIC PREFIX
 
-   % Construct the generic transmission pulse consisting of
-   % an impulse whose amplitude is dictated by the power 
-   % constraint, followed by K-1 zeros.
-   pulse = (n*P/numbits)^.5*[1; zeros(K-1,1)];
-
-   % Build the transmitted signal. We send a +/- impulse, 
-   % with amplitude dictated by the power constraint, for
-   % each bit. Between pulses, we send K-1 zeros for the
-   % channel to settle down.
-   x = [];
-   for i = 1:numbits,
-      x = [x; pulse*(2*bits(i) - 1)];
-   end
    
    %We need to create a wav file from x. Spec'd by project.
-   audiowrite('tx.wav', x, 44100, 'BitsPerSample', 24);
+   audiowrite('tx.wav', x, 44100, 'BitsPerSample', K);
    
 return 
