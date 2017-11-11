8a47d25dec2ff9781b72faf10ef4aa2941bf7a2f
Tian (Tim) Yao <tiansky.yao95@gmail.com>
2017-11-11 16:13:01 -0500

    Added channel sim and channel impulse response

---
+++ chansim.m
@@ -0,0 +1,15 @@
+function [ output ] = chansim( input )
+%CHANSIM Summary of this function goes here
+%   Detailed explanation goes here
+
+    % Load impulse response
+	load('IR0.mat');
+    
+    % Convolve input signal with impulse response
+    afterfilter = conv(impulse, input);
+    
+    % Add white noise
+    snr = 100;
+    output = awgn(afterfilter, snr);
+end
+
