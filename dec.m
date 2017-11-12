fe91a2813e07a474bf2425d0122f387ee9be8807
Joe <jmd456@cornell.edu>
2017-11-12 17:24:42 -0500

    Add files via upload
    Derpderp

--- dec.m
+++ dec.m
@@ -1,27 +1,41 @@
 function outbits = dec(y)
-   % Needed to decode from a wav file "rx"
-   % Spec'd by project, we can set it to a variable later
-   audioread('rx.wav', y, 44100, 'BitsPerSample', 24);
-   
-   % number of bits to send; 'outbits' will be a column vector of
-   % this size
-   numbits = 24;
+ numbits = 200000;
 
-   % time constant of the channel: approx. number of nonzero samples
-   % in the impulse response
-   K = 250;
-
-   % Generate the impulse response
-
-   tmp = (0:K-1)';
-   h = (-.99).^tmp;
-
-   % Use the minimum-distance rule to decode each bit separately.
-   outbits = [];
-
-   for i = 1:numbits,
-      outbits = [ outbits; (h'*y((i-1)*K+1:i*K) > 0)];
-   end
-   
+ %Power constraint
+ P=1/4;
+ %skinny version of the decoder
+ 
+ %take DFT of the recieved signal
+ Y = dft(y);
+ 
+ %remove bottom half
+ Y = Y(1:end/2);
+ 
+ %Drop DC component
+ Y = Y(2:end-1);
+ 
+ %ADD LAMBDA HERE
+ 
+ %DECODE REAL AND IMAG COMPONENTS
+ boundary = sqrt(P/2);
+ outbits = zeros(1, numbits);
+ for i = (1:length(Y))
+    if((real(Y(i)) >= boundary)&&(imag(Y(i))>= boundary))
+        outbits(i*2 - 1) = 1;
+        outbits(i*2) = 1;
+    end 
+    if((real(Y(i)) >= boundary)&&(imag(Y(i))<= boundary))
+        outbits(i*2 - 1) = 1;
+        outbits(i*2) = 0;
+    end 
+    if((real(Y(i)) <= boundary)&&(imag(Y(i))>= boundary))
+        outbits(i*2 - 1) = 0;
+        outbits(i*2) = 1;
+    end 
+    if((real(Y(i)) <= boundary)&&(imag(Y(i))<= boundary))
+        outbits(i*2 - 1) = 0;
+        outbits(i*2) = 0;
+    end 
+ end    
 
 end
